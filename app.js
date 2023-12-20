14491e7313246c87799f0d83f904ea7ad2fdc562
shiv <yshiv4573@gmail.com>
2023-12-20 13:06:46 +0530

    aa

--- app.js
+++ app.js
@@ -9,20 +9,6 @@ app.get('/',(req,res)=>{
 })
 app.use(cors);
 
-// const allowCors = fn => async (req, res) => {
-//     res.setHeader('Access-Control-Allow-Credentials', true)
-//     res.setHeader('Access-Control-Allow-Origin', '*')
-//     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-//     res.setHeader(
-//       'Access-Control-Allow-Headers',
-//       'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-//     )
-//     if (req.method === 'OPTIONS') {
-//       res.status(200).end()
-//       return
-//     }
-//     return await fn(req, res)
-//   }
 
 require("dotenv").config({path: "backend/config/config.env"})
 
@@ -30,4 +16,4 @@ const user = require("./routes/UserRoutes");
 
 app.use("/api",user);
 
-module.exports = allowCors(app);
+module.exports = app;
