c626c6bd91bcf18e32e73801b8c3fa9fc86176ef
shiv <yshiv4573@gmail.com>
2023-12-20 12:55:32 +0530

    a

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
 const express = require("express");
 const app = express();  
 require("dotenv").config({path: "./config.env"})
-
+const cors = require("cors");
 //Using middleware
 app.use(express.json());
 app.get('/',(req,res)=>{
     res.send('the api is running omg');
 })
+app.use(cors);
 
-const allowCors = fn => async (req, res) => {
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-    if (req.method === 'OPTIONS') {
-      res.status(200).end()
-      return
-    }
-    return await fn(req, res)
-  }
+// const allowCors = fn => async (req, res) => {
+//     res.setHeader('Access-Control-Allow-Credentials', true)
+//     res.setHeader('Access-Control-Allow-Origin', '*')
+//     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+//     res.setHeader(
+//       'Access-Control-Allow-Headers',
+//       'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+//     )
+//     if (req.method === 'OPTIONS') {
+//       res.status(200).end()
+//       return
+//     }
+//     return await fn(req, res)
+//   }
 
 require("dotenv").config({path: "backend/config/config.env"})
 
